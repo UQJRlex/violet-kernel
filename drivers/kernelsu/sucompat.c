b04a06676ce57f67a4425fbe457384f1c144f019
UQJRlex <xuzhuyin527@gmail.com>
2025-11-04 10:44:26 +0800

    KernelSU: add KernelSU-v1.0.5-105-12184

--- drivers/kernelsu/sucompat.c
+++ drivers/kernelsu/sucompat.c
@@ -120,12 +120,8 @@ static __always_inline bool is_su_allowed(const void *ptr_to_check)
 	if (!ksu_sucompat_non_kp)
 		return false;
 
-	// we disable seccomp on setuid hook anyway
-	if (!!current->seccomp.mode)
+	if (likely(!ksu_is_allow_uid(current_uid().val)))
 		return false;
-
-	//if (likely(!ksu_is_allow_uid(current_uid().val)))
-	//	return false;
 
 	if (unlikely(!ptr_to_check))
 		return false;
